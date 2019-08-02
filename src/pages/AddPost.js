94d8dad434fa302db70e1957fd4e785e6a5aa519
eserdinyo <muhammeteser29@gmail.com>
2019-08-02 21:11:33 +0300

    add form validation formik with yup

--- src/pages/AddPost.js
+++ src/pages/AddPost.js
@@ -1,13 +1,67 @@
 import React from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+const SignupSchema = Yup.object().shape({
+    firstName: Yup.string()
+        .min(5, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required'),
+    lastName: Yup.string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required'),
+    age: Yup.number()
+        .min(10, 'cok kisa')
+        .max(100, 'cok uzun')
+        .required('Gerekli'),
+    email: Yup.string()
+        .email('Geçerli bir mail adresi girin.')
+        .required('Required'),
+});
 
 const AddPost = () => {
     return (
         <div className='login'>
             <h3>Ilan Ekle</h3>
-            <input type="text" placeholder='Pozinyon' />
+            <Formik
+                initialValues={{
+                    firstName: '',
+                    lastName: '',
+                    email: '',
+                    age: '',
+                }}
+                validationSchema={SignupSchema}
+                onSubmit={values => {
+                    // same shape as initial values
+                    console.log(values);
+                }}
+            >
+                {({ errors, touched }) => (
+                    <Form className='login'>
+                        <Field name="firstName" placeholder="username" />
+                        {errors.firstName && touched.firstName ? (
+                            <div>{errors.firstName}</div>
+                        ) : null}
+                        <Field name="lastName" placeholder="lastname" />
+                        {errors.lastName && touched.lastName ? (
+                            <div>{errors.lastName}</div>
+                        ) : null}
+                        <Field name="age" placeholder="yas" />
+                        {errors.age && touched.age ? (
+                            <div>{errors.age}</div>
+                        ) : null}
+                        <Field name="email" type="email" placeholder="email" />
+                        {errors.email && touched.email ? <div>{errors.email}</div> : null}
+                        <button type="submit">Submit</button>
+                    </Form>
+                )}
+            </Formik>
+
+            {/* <input type="text" placeholder='Pozinyon' />
             <input type="text" placeholder='Şirket' />
             <input type="text" placeholder='Lokasyon' />
-            <button className="btn"> Kaydet </button>
+            <button className="btn"> Kaydet </button> */}
         </div>
     )
 }
